c0c2b1a787ce7f1d7e652ff099279fac5e3909d7
Dipankar Pal <dipankarpal5050@gmail.com>
2020-09-06 20:07:07 +0530

    First Upload

---
+++ app/renderer/index.js
@@ -0,0 +1,66 @@
+const { ipcRenderer } = require('electron')
+
+
+// console.log("hiiiii")
+// document.getElementById('drag').ondragstart = (event) => {
+//     console.log("joooooo");
+//     event.preventDefault()
+//     ipcRenderer.send('ondragstart', '/path/to/item')
+//   }
+
+
+/////////////////
+var file_paths = "";
+var dropArea = document.getElementById('drop');
+
+dropArea.addEventListener('drop', (event) => {
+    file_paths = "";
+    // console.log("hello");
+    event.preventDefault();
+    event.stopPropagation();
+
+    let count = 0;
+
+    for (const f of event.dataTransfer.files) {
+        // Using the path attribute to get absolute file path 
+        console.log('File Path of dragged files: ', f.path)
+        file_paths = file_paths + "\n" + f.path.toString();
+        count++;
+    }
+    document.getElementById('count').innerText = count.toString() + " Item(s) Selected";
+});
+
+dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+});
+
+dropArea.addEventListener('dragenter', (event) => {
+    document.getElementById('count').innerText = "Umm, I See Some Items :) Drop 'em Now";
+});
+
+dropArea.addEventListener('dragleave', (event) => {
+    document.getElementById('count').innerText = "Drop Things Here To Get Started";
+});
+
+
+
+
+/////////////////////
+
+
+document.getElementById('go').addEventListener('click', (event) => {
+    document.getElementById('count').innerText = "Working On It ........";
+
+    if (file_paths != "") {
+        ipcRenderer.send('arrange', file_paths);
+    }
+
+    file_paths = "";
+
+});
+
+
+ipcRenderer.on('done', (event, msg) => {
+    document.getElementById('count').innerText = "DONE";
+});
---
+++ main.js
@@ -0,0 +1,159 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const url = require('url')
+const path = require('path')
+const FileTypes = require('./file-types.json')
+const fs = require('fs')
+
+
+
+
+require('electron-reload')(path.join(__dirname, 'app'));
+
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 700, // 300
+    height: 700,  // 400
+    resizable: true,
+    frame: false,
+    maximizable: false,
+    fullscreenable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'app', 'view', 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+
+
+  win.webContents.openDevTools()
+}
+
+
+app.whenReady().then(createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+
+
+
+
+/////////////////////////
+
+
+byType = (filePathArr, handleDirs) => {
+  var filesUnderAllDirs = [];
+  filePathArr.forEach(_path => {
+
+    if (fs.existsSync(_path)) {
+
+
+      if (fs.lstatSync(_path).isFile()) {
+        baseFileName = path.basename(_path);
+        // is file ??
+        var ext;
+        try {
+          ext = path.extname(_path).split('.')[1];
+        } catch (error) {
+          console.error("ERROR: " + _path + " no extension found");
+        }
+
+        if (ext && ext !== "") {
+          if (FileTypes[ext]) {
+            console.log(_path + " Is of Type " + FileTypes[ext]);
+
+            // logic goes here ..................
+
+            // extract the directory from the file path
+            currDirName = path.dirname(_path);
+
+            // create directory(UPPERCASE) if not exists based on the type
+            determinedDirName = FileTypes[ext].toUpperCase();
+            newDirPath = path.join(currDirName, determinedDirName);
+            movePath = path.join(newDirPath, baseFileName);
+
+            if (!fs.existsSync(newDirPath)) {
+
+              try {
+                fs.mkdirSync(newDirPath)
+              } catch (error) {
+                console.log("Error: create " + newDirPath);
+              }
+            }
+
+            // move file to the newly created directory
+            try {
+              fs.renameSync(_path, movePath);
+            } catch (error) {
+              console.log("Error: move " + movePath);
+            }
+
+          }
+          else {
+            console.log("ERROR: " + _path + " could not decide")
+          }
+        }
+      }
+      else if (fs.lstatSync(_path).isDirectory() && handleDirs == true) {
+        // if feature is enabled, list all the FILES  ( exclude further directories) i.e. depth = 1 and add it to a list
+
+        console.log(_path + " is a Directory : Listing all the files to arrange them :)");
+        dirPath = _path;
+        entries = fs.readdirSync(dirPath);
+
+        entries.forEach(element => {
+          if (fs.lstatSync(path.join(dirPath, element)).isFile()) {
+            filesUnderAllDirs.push(path.join(dirPath, element)); // appending files only
+          }
+        });
+      }
+    }
+  });
+
+  // now call this function again with handleDirs=false
+  if (filesUnderAllDirs && filesUnderAllDirs.length != 0) {
+    byType(filesUnderAllDirs, false);
+  }
+
+}
+
+
+
+
+
+var mode = 1;
+
+analyzeAndConvert = async (filePathArr) => {
+  if (mode === 1) {
+    byType(filePathArr, true);
+  }
+}
+
+
+
+ipcMain.on('arrange', (event, filePath) => {
+  // console.log(filePath);
+  var filePathArr = filePath.split("\n");
+  analyzeAndConvert(filePathArr);
+  let currWin = BrowserWindow.getFocusedWindow()
+  currWin.webContents.send('done', 'done');
+})
